4fb26206859d958427b76a0eefd4e6beb42c2ad2
Sicl <47028479+sicl5300@users.noreply.github.com>
2023-12-03 21:43:22 +0800

    revert

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,7 +3,7 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🎄 Apeiria's Garden",
+    pageTitle: "🎄 Quartz 4.0",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
